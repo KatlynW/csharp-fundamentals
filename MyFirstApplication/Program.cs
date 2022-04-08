5997704bde350db4b8c6b134d001437f541efd1b
Katlyn Williams <frogkatlyn@gmail.com>
2022-04-07 20:50:26 -0500

    CAC - 103 Lesson 1 Value Types

--- MyFirstApplication/Program.cs
+++ MyFirstApplication/Program.cs
@@ -1,11 +1,13 @@
 ﻿using MyFirstApplication;
 
 // See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+// Console.WriteLine("Hello, World!");
 
 // The below statement calls a local function
 // single line comment
-HouseExample();
+
+// BottlesofBeer bottles = new BottlesofBeer();
+// bottles.LetsSing();
 
 /*
  Multi-line comment
@@ -14,12 +16,28 @@ are inside of members and are private by default. This means
 they are specific to that member and can only be called
 inside that member.
  */
+
+
+// Lesson Examples
+// HouseExample();
+// MyLocalFunction();
+Lesson1Example();
+
 void HouseExample()
 {
     House myHouse = new House();
     myHouse.DoorOpenClose();
 }
 
+// Lesson 1 Value Types
+void Lesson1Example()
+{
+    Lesson1 myLesson = new Lesson1();
+    myLesson.IntMinMax();
+    myLesson.IntegralExample();
+    myLesson.ConvertFloatToInt();
+    myLesson.LongFromInt();
+}
 
 /*
  This local function is for my first exercise lab.
@@ -31,5 +49,3 @@ void MyLocalFunction()
     Console.WriteLine("This Csharp course is cool");
     Console.WriteLine("I am learning stuff everyday.");
 }
-
-MyLocalFunction();
